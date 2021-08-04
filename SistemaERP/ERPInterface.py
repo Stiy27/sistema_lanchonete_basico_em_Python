38cab664c8b5fb59a905912548e8fb9b8b80256e
Walfrank Corrêa <stiy27@gmail.com>
2021-08-03 21:20:23 -0300

    Update ERPInterface.py
    Cadastro de Usuários BackEnd

--- SistemaERP/ERPInterface.py
+++ SistemaERP/ERPInterface.py
@@ -72,6 +72,57 @@ class LoginWindow():
                 # Chama/Abre a janela de Administrdor após login efetuado com sucesso como Master.
                 AdminWindow()
 
+# ----------------------------------------------------------------------------------------------------------------------
+    def CadastroBackEnd(self):
+        codigoPadrao = '123@h'
+
+        if self.codigoSeguranca.get() == codigoPadrao:
+            if len(self.login.get()) <= 20:
+                if len(self.password.get()) <= 50:
+                    nome = self.login.get().lower()
+
+                    senha = self.password.get()
+
+                    try:
+                        conexao = pymysql.connect(
+
+                            host='localhost',
+                            user='root',
+                            password='',
+                            db='erp',
+                            charset='utf8mb4',
+                            cursorclass=pymysql.cursors.DictCursor
+
+                        )
+                    except:
+                        # Colocar este print em MessageBox, ao compilar em '.EXE'
+                        messagebox.showinfo('Conexão DB', 'Erro ao conecta ao BD')
+
+                    try:
+                        with conexao.cursor() as cursor:
+                            cursor.execute('insert into cadastro values (%s, %s, %s)',(nome, senha, 1))
+                            conexao.commit()
+                        messagebox.showinfo('Cadastro', 'Usuário cadastrado com sucesso.')
+                        # Destroio a janela de cadastro/login para que o usuário entre com os novos dados cadastrados
+                        self.root.destroy()
+                    except:
+                        messagebox.showinfo('Cadastro', 'Erro ao cadastrar usuário')
+                else:
+                    messagebox.showinfo('ERRO', 'Digite a senha com o máximo de 50 caracteres')
+            else:
+                messagebox.showinfo('ERRO', 'Digite um nome com o máximo de 20 caracteres')
+        else:
+            messagebox.showinfo('ERRO', 'Código de segurança incorreto')
+
+# ---------------------------------------------------------------------------------------------------------------------
+    # Mostra o campo Çhave de SEgurança' ao clicar no botão cadastrar na janela de login
+    def Cadastro(self):
+        Label(self.root, text='Chave de Segurança').grid(row=3, column=0, padx=5, pady=5)
+        self.codigoSeguranca = Entry(self.root)
+        self.codigoSeguranca.grid(row=3, column=1, padx=10, pady=5)
+        # Efetua o cadastro após preenchido os campos e clicado no botão Confirmar cadastro/Cadastrar
+        Button(self.root, text='Confirmar cadastro', width=15, bg='blue1', command=self.CadastroBackEnd).grid(row=4, column=0, columnspan=3, pady=5, padx=10)
+
 #----------------------------------------------------------------------------------------------------------------------
 
     # Cria a Janela do App
@@ -96,13 +147,13 @@ class LoginWindow():
 
         # Cria o Botões para logar/entrar, cadastrar e ver cadastros no sistema
         botaoEntrar = Button(self.root, text='Entrar', bg='Dark Gray', height=1, width=8, command=self.VerificaLogin)
-        botaoEntrar.grid(row=3, column=0, padx=5, pady=5)
+        botaoEntrar.grid(row=5, column=0, padx=5, pady=5)
         #---
-        botaoCadastrar = Button(self.root, text='Cadastrar', bg='Dark Gray', height=1, width=8, command='')
-        botaoCadastrar.grid(row=3, column=1, padx=5, pady=5)
+        botaoCadastrar = Button(self.root, text='Cadastrar', bg='Dark Gray', height=1, width=8, command=self.Cadastro)
+        botaoCadastrar.grid(row=5, column=1, padx=5, pady=5)
         #--
         botaoViewCadastro = Button(self.root, text='Visualizar Cadastros', bg='White', height=1, width=18, command='')
-        botaoViewCadastro.grid(row=4, column=0, columnspan=2, padx=5, pady=8)
+        botaoViewCadastro.grid(row=6, column=0, columnspan=2, padx=5, pady=8)
 
 
 
