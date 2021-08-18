85b51b3277026274dbb6be70a95c3590273d5674
Walfrank Corrêa <stiy27@gmail.com>
2021-08-18 19:19:29 -0300

    Update ERPInterface.py
    Inplementado função para visualizar usuários cadastrados

--- SistemaERP/ERPInterface.py
+++ SistemaERP/ERPInterface.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import messagebox
+from tkinter import ttk
 import pymysql
 
 # Cria janela de Administrador
@@ -124,6 +125,79 @@ class LoginWindow():
         Button(self.root, text='Confirmar cadastro', width=15, bg='blue1', command=self.CadastroBackEnd).grid(row=4, column=0, columnspan=3, pady=5, padx=10)
 
 #----------------------------------------------------------------------------------------------------------------------
+    # Recupera
+    def UpdateBackEnd(self):
+        try:
+            conexao = pymysql.connect(
+                host='localhost',
+                user='root',
+                password='',
+                db='erp',
+                charset='utf8mb4',
+                cursorclass=pymysql.cursors.DictCursor
+            )
+        except:
+            # Colocar este print em MessageBox, ao compilar em '.EXE'
+            print('Erro ao conectar ao Banco de Dados ERP')
+
+        # Pega todas as linha da Tabela Cadastro e armazena na variável resultado
+        try:
+            with conexao.cursor() as cursor:
+                cursor.execute('select * from cadastro')
+                resultados = cursor.fetchall()
+        except:
+            print('Erro na consulta')
+
+        # Apaga tudo o que estiver na Treeview para atualização de novos dados.
+        self.tree.delete(*self.tree.get_children())
+
+        # Variável para armazenar os dasdos de cada linha/registro do Banco de Dados
+        linhaV = []
+
+        # Laço para recuperar os dados do BD, armazenar na variável do tipo lista "linhaV" e exibir na Treview.
+        for linha in resultados:
+            linhaV.append(linha['id'])
+            linhaV.append(linha['nome'])
+            linhaV.append(linha['senha'])
+            linhaV.append(linha['nivel'])
+
+            # O parametro 'iid=' identifica o registro utilizando o campo especificado neste parametro
+            self.tree.insert("", END, values=linhaV, iid=linha['id'], tag='1')
+
+            linhaV.clear()
+
+#----------------------------------------------------------------------------------------------------------------------
+    def VisualizarCadastros(self):
+        # Cria a janela de visulaização de usuários cadastrados.
+        # É como uma instancia de TK(), porém só abre a partir da janela abaixo do TK()/self.root
+        # ser fechar a janela self.roo/login, a a janela self.vc/visualizar cadastro, também fecha, pois é Toplevel
+        # depende da root.
+        self.vc = Toplevel()
+        self.vc.resizable(False, False)
+        self.vc.title('Visualizar Cadastros')
+
+        # A self.tree/Treeview edita os campos onde os usuários cadastrados serão exibidos
+        # Fica dentro da janela self.vc.
+        self.tree = ttk.Treeview(self.vc, select="browse", column=("column1", "column2", "column3", "column4"),
+                                 show='headings')
+        self.tree.column("column1", width=30, minwidth=500, stretch=NO)
+        self.tree.heading('#1', text='ID')
+
+        self.tree.column("column2", width=60, minwidth=500, stretch=NO)
+        self.tree.heading('#2', text='Usuário')
+
+        self.tree.column("column3", width=60, minwidth=500, stretch=NO)
+        self.tree.heading('#3', text='Senha')
+
+        self.tree.column("column4", width=40, minwidth=500, stretch=NO)
+        self.tree.heading('#4', text='Nível')
+
+        self.tree.grid(row=0, column=0, padx=10, pady=10)
+
+        # Chama a função UpdateBackEnd() para atulizar a janela self.vc/Treeview.
+        self.UpdateBackEnd()
+
+        self.vc.mainloop()
 
     # Cria a Janela do App
     def __init__(self):
@@ -152,7 +226,8 @@ class LoginWindow():
         botaoCadastrar = Button(self.root, text='Cadastrar', bg='Dark Gray', height=1, width=8, command=self.Cadastro)
         botaoCadastrar.grid(row=5, column=1, padx=5, pady=5)
         #--
-        botaoViewCadastro = Button(self.root, text='Visualizar Cadastros', bg='White', height=1, width=18, command='')
+        botaoViewCadastro = Button(self.root, text='Visualizar Cadastros', bg='White', height=1, width=18,
+                                   command=self.VisualizarCadastros)
         botaoViewCadastro.grid(row=6, column=0, columnspan=2, padx=5, pady=8)
 
 
